083e9ec0dd44c27807534b21995540eefb16de57
Michael Grill <Ultramar@Michaels-MacBook-Pro.local>
2018-11-08 19:22:16 -0500

    figuringOutNeedForLoop

--- walmart.js
+++ walmart.js
@@ -1,7 +1,7 @@
 $("#submit").on("click", function() {
 
 
-var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + searchQuery;
+var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag&query=" + searchQuery + "";
 var searchQuery = $("#search").val();
 
 $.ajax({
@@ -9,12 +9,20 @@ $.ajax({
     method: "get",
 }).then(function(responce) {
 
+    for (var i = 0; i < responce.length; i++){
+
+        var thumbnailUrl = responce[i].thumbnailImage;
+
+        var productName = responce[i].name;
+
+        var price = responce[i].salesPrice;
+
+        
 
 
 
+    }
 
-
-    
 });
 
 
